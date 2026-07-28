de1019e5d1e32d41b0bea12c5ad9e087c645666b
Mahboo27 <gracieyugto@gmail.com>
2026-07-28 22:13:12 +0800

    Update deposit.js

--- src/commands/IponTracker/deposit.js
+++ src/commands/IponTracker/deposit.js
@@ -1,1 +1,21 @@
+const { SlashCommandBuilder } = require("discord.js");
 
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("deposit")
+        .setDescription("Deposit money into your savings")
+        .addNumberOption(option =>
+            option
+                .setName("amount")
+                .setDescription("Amount to deposit")
+                .setRequired(true)
+        ),
+
+    async execute(interaction) {
+        const amount = interaction.options.getNumber("amount");
+
+        await interaction.reply({
+            content: `💰 Successfully deposited **₱${amount.toLocaleString()}** into your savings!`,
+        });
+    },
+};
